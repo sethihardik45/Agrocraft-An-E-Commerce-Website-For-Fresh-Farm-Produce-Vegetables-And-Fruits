5551579df2d04d70f204400a03be7d0f38c525a3
Hardik Sethi <hardiksethi.bt22ele@pec.edu.in>
2025-03-04 23:36:10 +0530

    fiustt7wisgodig6

--- Includes/db.php
+++ Includes/db.php
@@ -1,14 +1,7 @@
 <?php 
 
-$host = "b5m9rkf7gmrn6f5udnjm-mysql.services.clever-cloud.com";       // Example: "bqfd56a3eqzv8p6wclevercloud.com"
-$user = "uehdcjhdrusou2ew";   // Example: "u8zqvxxng8hxcj9l"
-$pass = "4Ui0yfNKNavPM4is0fuI";   // Example: "Xyz123SecurePass"
-$dbname = "b5m9rkf7gmrn6f5udnjm";  // Example: "bqfd56a3eqzv8p6"
-
-$con = mysqli_connect($host, $user, $pass, $dbname);
-
-if (!$con) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-
-?>
+        $con = mysqli_connect("localhost","root","","agrocraft");
+      
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySql " . mysqli_connect_error();
+        }
